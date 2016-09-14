b93181a65a40054d73b2d9ff3279577050038bc4
Matteo Romagnoli <matteo.romagnoli@miniclip.com>
2016-09-15 00:47:42 +0200

    Read a json file from StreamingAssets and load in a struct

---
+++ lottery-simulator/Assets/Scripts/LotteryFileLoader.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LotteryFileLoader : MonoBehaviour {
+
+	#region Data
+	[System.Serializable]
+	public struct MasterDataObject
+	{
+		[System.Serializable]
+		public struct MasterDataEntry
+		{
+			public int start;
+			public int end;
+			public string owner;
+		}
+
+		public MasterDataEntry[] list;
+	}
+	#endregion
+
+	#region Public Fields
+	public string fileName;
+	#endregion
+
+	#region Unity Callbacks
+	IEnumerator Start ()
+	{
+		var path = System.IO.Path.Combine ("file://" + Application.streamingAssetsPath, fileName);
+		WWW www = new WWW (path);
+		yield return www;
+
+		MasterDataObject mdo = JsonUtility.FromJson<MasterDataObject> (www.text);
+		Debug.Log ("First: start " + mdo.list [0].start + ", end " + mdo.list [0].end + ", owner " + mdo.list [0].owner);
+
+	}
+
+	void Update ()
+	{
+	
+	}
+	#endregion
+}
